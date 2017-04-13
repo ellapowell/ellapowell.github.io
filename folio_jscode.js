d38c45dab5448cc42f991ffafd37bea8c0f77c28
ellapowell <hgf.jhfv>
2017-04-13 18:02:11 +1200

    javascript code

---
+++ folio_jscode.js
@@ -0,0 +1,62 @@
+
+function newpicture(){
+  document.getElementById ("image").src="photos for web/6/row1first-imgover.jpg"
+}
+
+function newpicture1(){
+  document.getElementById ("image1").src="photos for web/6/row1m-imgover.jpg"
+}
+
+function newpicture2(){
+  document.getElementById ("image2").src="photos for web/6/row1end-imgover.jpg"
+}
+
+function newpicture3(){
+  document.getElementById ("image3").src="photos for web/4/row2first-imgover.jpg"
+}
+
+function newpicture4(){
+  document.getElementById ("image4").src="photos for web/4/row2m-imgover.jpg"
+}
+
+function newpicture5(){
+  document.getElementById ("image5").src="photos for web/4/row2end-imgover.jpg"
+}
+
+function newpicture6(){
+  document.getElementById ("image6").src="photos for web/5/row3first-imgover.jpg"
+}
+
+function newpicture7(){
+  document.getElementById ("image7").src="photos for web/5/row3m-imgover.jpg"
+}
+
+function newpicture8(){
+  document.getElementById ("image8").src="photos for web/5/row3end-imgover.jpg"
+}
+
+
+function newpicture9(){
+  document.getElementById ("image9").src="photos for web/8/row4first-imgover.jpg"
+}
+
+function newpicture10(){
+  document.getElementById ("image10").src="photos for web/8/row4m-imgover.jpg"
+}
+
+function newpicture11(){
+  document.getElementById ("image11").src="photos for web/8/row4end-imgover.jpg"
+}
+
+
+function newpicture12(){
+  document.getElementById ("image12").src="photos for web/3/row5first-imgover.jpg"
+}
+
+function newpicture13(){
+  document.getElementById ("image13").src="photos for web/3/row5m-imgover.jpg"
+}
+
+function newpicture14(){
+  document.getElementById ("image14").src="photos for web/3/row5end-imgover.jpg"
+}
